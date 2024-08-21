a9afe072f47f00a55cd331e986996756a1ac8b7a
Beyzanur Bayır <bayirbeyzanur574@gmail.com>
2024-08-21 14:25:08 +0300

    çakışma sorunları çözüldü

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eacf2b9a900e5987886779f2877d955d5bcc69bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d7dc7fe25b232c616d4a89988cfbb1ea83dba7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
